c9a2222073b909f9d89245049ca08e9bbf305b21
kmalap05 <83391228+kmalap05@users.noreply.github.com>
2022-11-07 00:42:25 +0530

    UI Changes

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -9,40 +9,69 @@ function Signin() {
 
     const [email, setEmail] = useState()   /* Instead of Using useState hook we can use useRef and keep ref in the required input field for getting data */
     const [password, setPassword] = useState()
-    const [error,setError] = useState("")
-    const { isFetching, dispatch } = useContext(AuthContext)
+    const [error, setError] = useState( "" )
+    const { isFetching, dispatch } = useContext( AuthContext )
 
     const API_URL = process.env.REACT_APP_API_URL
 
-    const loginCall = async (userCredential, dispatch) => {
-        dispatch({ type: "LOGIN_START" });
+    const loginCall = async ( userCredential, dispatch ) => {
+        dispatch( { type: "LOGIN_START" } );
         try {
-            const res = await axios.post(API_URL+"api/auth/signin", userCredential);
-            dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+            const res = await axios.post( API_URL + "api/auth/signin", userCredential );
+            dispatch( { type: "LOGIN_SUCCESS", payload: res.data } );
         }
-        catch (err) {
-            dispatch({ type: "LOGIN_FAILURE", payload: err })
-            setError("Wrong Password Or Username")
+        catch ( err ) {
+            dispatch( { type: "LOGIN_FAILURE", payload: err } )
+            setError( "Wrong Password Or Username" )
         }
     }
 
-    const handleForm = (e) => {
+    const handleForm = ( e ) => {
         e.preventDefault()
-        loginCall({ email, password }, dispatch)
+        loginCall( { email, password }, dispatch )
     }
 
     return (
+        // ----------------OLD CODE-----------------------
+        // <div className='signin-container'>
+        //     <div className="signin-card">
+        //         <form onSubmit={handleForm}>
+        //             <h2 className="signin-title"> Log in</h2>
+        //             <p className="line"></p>
+        //             <div className="error-message"><p>{error}</p></div>
+        //             <div className="signin-fields">
+        //                 <label htmlFor="email"> <b>Email</b></label>
+        //                 <input className='signin-textbox' type="email" placeholder="Enter Email" name="username" required onChange={(e) => { setEmail(e.target.value) }} />
+        //                 <label htmlFor="password"><b>Password</b></label>
+        //                 <input className='signin-textbox' type="password" minLength='6' placeholder="Enter Password" name="psw" required onChange={(e) => { setPassword(e.target.value) }} />
+        //             </div>
+        //             <button className="signin-button" disabled={isFetching}>{isFetching ? <CircularProgress color="#ffffff" /> : "Log In"}</button>
+        //             <a className="forget-pass" href="#home">Forgot password?</a>
+        //         </form>
+        //         <div className='signup-option'>
+        //             <p className="signup-question">Don't have an account? <Link to='/signup'>Sign Up</Link></p>
+        //         </div>
+        //     </div>
+        // </div>
+        // -------------------------------------------------
+
         <div className='signin-container'>
+            <div class="left">
+                <div class="overlay">
+                    <div className='content'> <h1>Join us with Amigochat.</h1>
+                    </div>
+                </div>
+            </div>
             <div className="signin-card">
                 <form onSubmit={handleForm}>
-                    <h2 className="signin-title"> Log in</h2>
+                    <h2 className="signin-title"> Log In</h2>
                     <p className="line"></p>
                     <div className="error-message"><p>{error}</p></div>
                     <div className="signin-fields">
                         <label htmlFor="email"> <b>Email</b></label>
-                        <input className='signin-textbox' type="email" placeholder="Enter Email" name="username" required onChange={(e) => { setEmail(e.target.value) }} />
+                        <input className='signin-textbox' type="email" placeholder="Enter Email" name="username" required onChange={( e ) => { setEmail( e.target.value ) }} />
                         <label htmlFor="password"><b>Password</b></label>
-                        <input className='signin-textbox' type="password" minLength='6' placeholder="Enter Password" name="psw" required onChange={(e) => { setPassword(e.target.value) }} />
+                        <input className='signin-textbox' type="password" minLength='6' placeholder="Enter Password" name="psw" required onChange={( e ) => { setPassword( e.target.value ) }} />
                     </div>
                     <button className="signin-button" disabled={isFetching}>{isFetching ? <CircularProgress color="#ffffff" /> : "Log In"}</button>
                     <a className="forget-pass" href="#home">Forgot password?</a>
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -9,42 +9,76 @@ function Signup() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [username, setUsername] = useState();
-  const [photo, setPhoto] = useState("");
-  const [error,setError] = useState("");
-  const [isLoading,setIsLoading] = useState(false)
+  const [photo, setPhoto] = useState( "" );
+  const [error, setError] = useState( "" );
+  const [isLoading, setIsLoading] = useState( false )
   const history = useHistory();
 
   const API_URL = process.env.REACT_APP_API_URL
 
-  const handleForm = async (e) => {
+  const handleForm = async ( e ) => {
     e.preventDefault();
-    setIsLoading(true)
+    setIsLoading( true )
     const config = {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     };
     const data = new FormData();
-    data.append("username", username);
-    data.append("email", email);
-    data.append("password", password);
+    data.append( "username", username );
+    data.append( "email", email );
+    data.append( "password", password );
 
-    if (photo !== "") {
-      data.append("photo", photo);
+    if ( photo !== "" ) {
+      data.append( "photo", photo );
     }
 
     try {
-      await axios.post(API_URL+"api/auth/signup", data, config);
-      history.push("/signin");
-    } catch (err) {
-      console.log(err);
-      setError("Username Already Exist")
+      await axios.post( API_URL + "api/auth/signup", data, config );
+      history.push( "/signin" );
+    } catch ( err ) {
+      console.log( err );
+      setError( "Username Already Exist" )
     }
-    setIsLoading(false)
+    setIsLoading( false )
   };
 
   return (
+    // ----------------OLD CODE-----------------------
+    // <div className="signup-container">
+    //   <div className="signup-card">
+    //     <form onSubmit={handleForm}>
+    //       <h2 className="signup-title"> Register </h2>
+    //       <p className="line"></p>
+    //       <div className="error-message"><p>{error}</p></div>
+    //       <div className="signup-fields">
+    //         <label htmlFor="username"> {" "} <b>Username</b></label>
+    //         <input className="signup-textbox" type="text" placeholder="Enter Username" name="username" required onChange={(e) => { setUsername(e.target.value); }} />
+    //         <label htmlFor="email">{" "}<b>Email</b></label>
+    //         <input className="signup-textbox" type="email" placeholder="Enter Email" name="email" required onChange={(e) => { setEmail(e.target.value); }} />
+    //         <label htmlFor="password"> <b>Password</b></label>
+    //         <input className="signup-textbox" type="password" placeholder="Enter Password" minLength="6" name="psw" required onChange={(e) => { setPassword(e.target.value); }} />
+    //         <label><b>Image(You can add Image later)</b></label>
+    //         <input className="file-input" type="file" accept=".png, .jpg, .jpeg, .gif" name="photo" onChange={(e) => { setPhoto(e.target.files[0]); }} />
+    //       </div>
+    //       <button className="signup-button" disabled={isLoading}>{isLoading ?<CircularProgress color="#ffffff" size="18px"/> : "Sign Up"}</button>
+    //     </form>
+    //     <div className="signup-option">
+    //       <p className="signup-question">
+    //         Have an account? <Link to="/signin">Sign In</Link>
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
+    // -------------------------------------------------
+
     <div className="signup-container">
+      <div class="left1">
+        <div class="overlay">
+          <div className='content'> <h1>Join us with Amigochat.</h1>
+          </div>
+        </div>
+      </div>
       <div className="signup-card">
         <form onSubmit={handleForm}>
           <h2 className="signup-title"> Register </h2>
@@ -52,15 +86,15 @@ function Signup() {
           <div className="error-message"><p>{error}</p></div>
           <div className="signup-fields">
             <label htmlFor="username"> {" "} <b>Username</b></label>
-            <input className="signup-textbox" type="text" placeholder="Enter Username" name="username" required onChange={(e) => { setUsername(e.target.value); }} />
+            <input className="signup-textbox" type="text" placeholder="Enter Username" name="username" required onChange={( e ) => { setUsername( e.target.value ); }} />
             <label htmlFor="email">{" "}<b>Email</b></label>
-            <input className="signup-textbox" type="email" placeholder="Enter Email" name="email" required onChange={(e) => { setEmail(e.target.value); }} />
+            <input className="signup-textbox" type="email" placeholder="Enter Email" name="email" required onChange={( e ) => { setEmail( e.target.value ); }} />
             <label htmlFor="password"> <b>Password</b></label>
-            <input className="signup-textbox" type="password" placeholder="Enter Password" minLength="6" name="psw" required onChange={(e) => { setPassword(e.target.value); }} />
+            <input className="signup-textbox" type="password" placeholder="Enter Password" minLength="6" name="psw" required onChange={( e ) => { setPassword( e.target.value ); }} />
             <label><b>Image(You can add Image later)</b></label>
-            <input className="file-input" type="file" accept=".png, .jpg, .jpeg, .gif" name="photo" onChange={(e) => { setPhoto(e.target.files[0]); }} />
+            <input className="file-input" type="file" accept=".png, .jpg, .jpeg, .gif" name="photo" onChange={( e ) => { setPhoto( e.target.files[0] ); }} />
           </div>
-          <button className="signup-button" disabled={isLoading}>{isLoading ?<CircularProgress color="#ffffff" size="18px"/> : "Sign Up"}</button>
+          <button className="signup-button" disabled={isLoading}>{isLoading ? <CircularProgress color="#ffffff" size="18px" /> : "Sign Up"}</button>
         </form>
         <div className="signup-option">
           <p className="signup-question">
